bfbe52973eef3d16165624afc76383e0add8e268
Frederic Font <frederic.font@upf.edu>
2018-07-24 10:22:41 +0200

    Reimplement MIDI part with webmidi

--- editor/midi.js
+++ editor/midi.js
@@ -1,136 +1,161 @@
 
 /* MIDI */
 
-var midiAccess;
-var midiOutputDeviceID = undefined;
-var midiInputDeviceID = undefined;
-var midiChannel = 0;
-var CONTROL_CHANGE_MIDI_VALUE = 176;
-var PROGRAM_CHANGE_MIDI_VALUE = 192;
+var ENABLE_SYSEX = false;
+var midiOutputDevice = undefined;
+var midiInputDevice = undefined;
+var midiChannel = undefined;
+
 
 function initMIDI(controlsElement){
 
-    if (!navigator.requestMIDIAccess) {
-        console.error('Ouch! WebMIDI is not supported in this browser. Please try with Google Chrome.')
-        return
-    }
+    WebMidi.enable(function (err) {
+        if (err) {
+            console.log("WebMidi could not be enabled. Please try using Google Chrome.", err);
+        } else {
+            console.log("WebMidi enabled!");
 
-    navigator.requestMIDIAccess()
-        .then(onMIDISuccess, onMIDIFailure);
+            // Connect MIDI input devices and create controls
+            var midiInputControlsDiv = document.createElement("div");
+            
+            var midiInputsLabel = document.createElement("label");
+            midiInputsLabel.innerHTML = 'MIDI input: ';
+            midiInputsLabel.htmlFor = 'midiInputsSelect';
+            
+            var midiInputsSelect = document.createElement("select");
+            midiInputsSelect.id = 'midiInputsSelect';
+            midiInputsSelect.onchange = function(){
+                setMIDIInputDevice(WebMidi.getInputById(document.getElementById("midiInputsSelect").value));
+            };
 
-    function onMIDISuccess(midiAccess_) {
-
-        midiAccess = midiAccess_;
-
-        // Connect MIDI input devices and create controls
-        var midiInputControlsDiv = document.createElement("div");
-        
-        var midiInputsLabel = document.createElement("label");
-        midiInputsLabel.innerHTML = 'MIDI input: ';
-        midiInputsLabel.htmlFor = 'midiInputsSelect';
-        
-        var midiInputsSelect = document.createElement("select");
-        midiInputsSelect.id = 'midiInputsSelect';
-        midiInputsSelect.onchange = function(){
-            midiInputDeviceID = document.getElementById("midiInputsSelect").value;
-        };
-
-        for (var input of midiAccess.inputs.values()){
             var option = document.createElement("option");
-            option.text = input.name;
-            option.value = input.id;
+            option.text = 'none';
+            option.value = '-1';
             midiInputsSelect.add(option);
-            if (midiInputDeviceID === undefined){
-                midiInputDeviceID = input.id; // Assign first
+            for (var input of WebMidi.inputs){
+                var option = document.createElement("option");
+                option.text = input.name;
+                option.value = input.id;
+                if (input.name.indexOf('Deckard') !== -1){  // If "Deckard" is in MIDI device name, assign it by default
+                    option.selected = 'selected';
+                    setMIDIInputDevice(input);
+                }
+                midiInputsSelect.add(option);
             }
-            input.onmidimessage = getMIDIMessage; // Bind onmidimessage function (we filter by devices in the function itself)
-        }
 
-        midiInputControlsDiv.appendChild(midiInputsLabel)
-        midiInputControlsDiv.appendChild(midiInputsSelect)
-        controlsElement.appendChild(midiInputControlsDiv)
+            midiInputControlsDiv.appendChild(midiInputsLabel)
+            midiInputControlsDiv.appendChild(midiInputsSelect)
+            controlsElement.appendChild(midiInputControlsDiv)
 
-        // Connect MIDI output devices and create controls
-        var midiOutputControlsDiv = document.createElement("div");
-        
-        var midiOutputsLabel = document.createElement("label");
-        midiOutputsLabel.innerHTML = 'MIDI output: ';
-        midiOutputsLabel.htmlFor = 'midiOutputsSelect';
-        
-        var midiOutputsSelect = document.createElement("select");
-        midiOutputsSelect.id = 'midiOutputsSelect';
-        midiOutputsSelect.onchange = function(){
-            midiOutputDeviceID = document.getElementById("midiOutputsSelect").value;
-        };
+            
+            // Connect MIDI output devices and create controls
+            var midiOutputControlsDiv = document.createElement("div");
+            
+            var midiOutputsLabel = document.createElement("label");
+            midiOutputsLabel.innerHTML = 'MIDI output: ';
+            midiOutputsLabel.htmlFor = 'midiOutputsSelect';
+            
+            var midiOutputsSelect = document.createElement("select");
+            midiOutputsSelect.id = 'midiOutputsSelect';
+            midiOutputsSelect.onchange = function(){
+                setMIDIOutputDevice(WebMidi.getOutputById(document.getElementById("midiOutputsSelect").value));
+            };
 
-        for (var output of midiAccess.outputs.values()){
             var option = document.createElement("option");
-            option.text = output.name;
-            option.value = output.id;
+            option.text = 'none';
+            option.value = '-1';
             midiOutputsSelect.add(option);
-            if (midiOutputDeviceID === undefined){
-                midiOutputDeviceID = output.id; // Assign first
+            for (var output of WebMidi.outputs){
+                var option = document.createElement("option");
+                option.text = output.name;
+                option.value = output.id;
+                if ((output.name.indexOf('Deckard') !== -1) || (midiOutputDevice === undefined)){
+                    // If "Deckard" is in MIDI device name, assign it by default
+                    option.selected = 'selected';
+                    setMIDIOutputDevice(output);
+                }
+                midiOutputsSelect.add(option);
             }
-        }
 
-        var midiChannelSelect = document.createElement("select");
-        midiChannelSelect.id = 'midiChannelSelect';
-        midiChannelSelect.onchange = function(){
-            midiChannel = parseInt(document.getElementById("midiChannelSelect").value, 10);
-        };
+            var midiChannelSelect = document.createElement("select");
+            midiChannelSelect.id = 'midiChannelSelect';
+            midiChannelSelect.onchange = function(){
+                midiChannel = document.getElementById("midiChannelSelect").value;
+            };
 
-        for (var i=0; i<16; i++){
             var option = document.createElement("option");
-            option.text = i + 1;
-            option.value = i;
+            option.text = 'all';
+            option.value = 'all';
+            option.selected = 'selected';
+            midiChannel = 'all';
             midiChannelSelect.add(option);
-        }
+            for (var i=0; i<16; i++){
+                var option = document.createElement("option");
+                option.text = i + 1;
+                option.value = i + 1;
+                midiChannelSelect.add(option);
+            }
 
-        midiOutputControlsDiv.appendChild(midiOutputsLabel)
-        midiOutputControlsDiv.appendChild(midiOutputsSelect)
-        midiOutputControlsDiv.appendChild(midiChannelSelect)
-        controlsElement.appendChild(midiOutputControlsDiv)        
+            midiOutputControlsDiv.appendChild(midiOutputsLabel)
+            midiOutputControlsDiv.appendChild(midiOutputsSelect)
+            midiOutputControlsDiv.appendChild(midiChannelSelect)
+            controlsElement.appendChild(midiOutputControlsDiv)  
+        }
+    }, ENABLE_SYSEX);
+}
+
+function setMIDIInputDevice(device){
+    if (midiInputDevice !== undefined){
+        midiInputDevice.removeListener();
     }
 
-    function onMIDIFailure() {
-        console.error('Could not access your MIDI devices.');
+    if (device !== false) {
+        midiInputDevice = device;
+        midiInputDevice.addListener('controlchange', "all", function(e) {
+            receiveControlChangeMessage(e.controller.number, e.value, e.channel);
+        });
+    } else {
+        // If device is false, the "none" option was selected
+        midiInputDevice = undefined;
+    }
+}
+
+function setMIDIOutputDevice(device){
+    if (device !== false){
+        midiOutputDevice = device;    
+    } else {
+        midiOutputDevice = undefined;
     }
 }
 
 function sendMIDIProgramChange(pcNumber, channel){
-    // http://www.onicos.com/staff/iz/formats/midi-event.html
-
-    if (channel === undefined){ channel = midiChannel; }
-    if (pcNumber !== undefined && midiOutputDeviceID !== undefined){
-        var output = midiAccess.outputs.get(midiOutputDeviceID);
-        var message = [PROGRAM_CHANGE_MIDI_VALUE + channel, pcNumber];  // Channel 1 only (TODO: support other channels)
-        output.send(message);   
+    if (channel === undefined) { channel = midiChannel };
+    if (midiOutputDevice !== undefined){
+        midiOutputDevice.sendProgramChange(pcNumber, channel);
+        console.log(`Sent MIDI PC message to ch${channel} ${pcNumber}`);    
     }
 }
 
-function sendMIDIControlChange(ccNumber, value, channel){
-    // http://www.onicos.com/staff/iz/formats/midi-event.html
-
-    if (channel === undefined){ channel = midiChannel; }
-    if (ccNumber !== undefined && midiOutputDeviceID !== undefined){
-        var output = midiAccess.outputs.get(midiOutputDeviceID);
-        var message = [CONTROL_CHANGE_MIDI_VALUE + channel, ccNumber, value];
-        output.send(message);
-        console.log(`Sent MIDI CC message to ch${channel + 1} ${ccNumber} ${value} `);   
+function sendMIDIControlChange(ccNumber, ccValue, channel){
+    if (channel === undefined) { channel = midiChannel };
+    if (midiOutputDevice !== undefined){
+        midiOutputDevice.sendControlChange(ccNumber, ccValue, channel);
+        console.log(`Sent MIDI CC message to ch${channel} ${ccNumber} ${ccValue}`);
     }
 }
 
 LAST_RECEIVED_CC_VALUES = {};
 FILTER_THRESHOLD = 5;
 BUFFER_LENGTH = 10;
-function shouldPassMessage(cc_number, cc_value){    
+function shouldPassControlChangeMessage(ccNumber, ccValue, channel){    
     var pass;
+    var key = `${ccNumber}-${channel}`;
 
-    if (!(cc_number in LAST_RECEIVED_CC_VALUES)){
-        LAST_RECEIVED_CC_VALUES[cc_number] = [];
-        pass = false;  // Don't pass first time message for a cc_number is received
+    if (!(key in LAST_RECEIVED_CC_VALUES)){
+        LAST_RECEIVED_CC_VALUES[key] = [];
+        pass = false;  // Don't pass first time message for a ccNumber is received
     } else {
-        var lastReceivedValues = LAST_RECEIVED_CC_VALUES[cc_number];
+        var lastReceivedValues = LAST_RECEIVED_CC_VALUES[key];
         var difference = Math.abs(lastReceivedValues[0] - lastReceivedValues[lastReceivedValues.length - 1]);
         if (difference >= FILTER_THRESHOLD){
             // Only if the difference with the value of previous Nth message is higher than threshold
@@ -142,32 +167,24 @@ function shouldPassMessage(cc_number, cc_value){
             pass = false;
         }
     }
-    LAST_RECEIVED_CC_VALUES[cc_number].push(cc_value);
-    if (LAST_RECEIVED_CC_VALUES[cc_number].length > BUFFER_LENGTH){
-        LAST_RECEIVED_CC_VALUES[cc_number] = LAST_RECEIVED_CC_VALUES[cc_number].slice(LAST_RECEIVED_CC_VALUES[cc_number].length - BUFFER_LENGTH);
+    LAST_RECEIVED_CC_VALUES[key].push(ccValue);
+    if (LAST_RECEIVED_CC_VALUES[key].length > BUFFER_LENGTH){
+        LAST_RECEIVED_CC_VALUES[key] = LAST_RECEIVED_CC_VALUES[key].slice(LAST_RECEIVED_CC_VALUES[key].length - BUFFER_LENGTH);
     }
     return pass;
 }
 
-function midiMessageIsControlChange(message){
-    return (message.data[0] >= CONTROL_CHANGE_MIDI_VALUE && message.data[0] < CONTROL_CHANGE_MIDI_VALUE + 16);
-}
-
-function getMIDIMessage(message){
-    if (message.target.id === midiInputDeviceID){  // Only process messages from selected input
-
-        var allowMessage = true;
-        if (midiMessageIsControlChange(message)){
-            // Apply filtering due to bug in Deckard's Dream MIDI out (sliders jitter)
-            allowMessage = shouldPassMessage(message.data[1], message.data[2])
+function receiveControlChangeMessage(ccNumber, ccValue, channel){
+   
+    // Apply filtering due to bug in Deckard's Dream MIDI out (sliders jitter)
+    var allowMessage = shouldPassControlChangeMessage(ccNumber, ccValue, channel)
+    
+    if (allowMessage){
+        console.log(`Received MIDI CC message from ch${channel} ${ccNumber} ${ccValue}`);
+        if (PRESET_MANAGER.currentPreset){
+            PRESET_MANAGER.currentPreset.receiveControlChange(ccNumber, ccValue);    
         }
-        if (allowMessage){
-            console.log('Received MIDI message with bytes: ' + message.data);
-            if (PRESET_MANAGER.currentPreset && midiMessageIsControlChange(message)){
-                PRESET_MANAGER.currentPreset.receiveControlChange(message.data[1], message.data[2]);    
-            }
-        } else {
-            //console.log('Discarded message')
-        }
+    } else {
+        //console.log('Discarded message')
     }
 }
